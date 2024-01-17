05a3c7638088a7dffad9b47d529d3f06d06534fa
Machad3x <machad3x@gmail.com>
2024-01-16 21:41:22 -0400

    sm8450-common: Set renderengine to SkiaGL Threaded

--- common.mk
+++ common.mk
@@ -211,6 +211,8 @@ PRODUCT_PACKAGES += \
 
 USE_OPENGL_RENDERER := true
 
+USE_OPENGL_RENDERER := true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
