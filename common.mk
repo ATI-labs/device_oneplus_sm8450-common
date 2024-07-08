a4a78dd52fdf6e136a54e44bf8854b11cf31b743
someone5678 <someone5678@users.noreply.github.com>
2024-07-08 14:23:40 +0900

    sm8450-common: Build vendor.qti.hardware.capabilityconfigstore@1.0
    Log:
    07-08 14:11:18.637  1300  1300 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    07-08 14:11:18.724  1366  1366 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/vppservice": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by /vendor/lib64/libvpplibrary.so in namespace (default)
    07-08 14:11:23.638  3100  3100 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    07-08 14:11:28.644  3177  3177 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    07-08 14:11:33.651  3194  3194 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    07-08 14:11:28.395  5816  5816 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    07-08 14:11:45.348  7112  7112 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    
    Change-Id: Ie8eae745e4b9e14ff6456d3b6992ae44087036f6

--- common.mk
+++ common.mk
@@ -200,6 +200,11 @@ PRODUCT_BOOT_JARS += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Configstore
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0 \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
