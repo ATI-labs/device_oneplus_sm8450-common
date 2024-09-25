82b1e2dcecc3a406f6117486dd84b39316403696
Machad3x <machad3x@gmail.com>
2024-09-25 14:18:46 -0400

    sm8450-common: init: Set vm_swappiness to 10

--- init/init.kernel.post_boot-taro.sh
+++ init/init.kernel.post_boot-taro.sh
@@ -72,7 +72,7 @@ function configure_zram_parameters() {
 		echo 8 > /sys/block/zram0/max_comp_streams
 		mkswap /dev/block/zram0
 		swapon /dev/block/zram0 -p 32758
-		echo 100 > /proc/sys/vm/swappiness
+		echo 10 > /proc/sys/vm/swappiness
 		echo 15 > /proc/sys/vm/dirty_background_ratio
 		echo 200 > /proc/sys/vm/vfs_cache_pressure
 		echo 3000 > /proc/sys/vm/dirty_writeback_centisecs
